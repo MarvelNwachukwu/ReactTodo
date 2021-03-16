064a3d262484066a35f709e41b872cb7c4b49464
Marvellous Nwachukwu <marvellousn561@gmail.com>
2021-03-16 11:39:29 +0100

    created signUp

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,48 +4,57 @@ import './scss/signin.scss';
 const SignIn = (props) => {
   return (
     <React.Fragment>
-      <h2 id='welcome'>Welcome back 🙂</h2>
-      <p id='signinInstruction'>To continue with your account, please login</p>
+      <div className='leftDivSignIn'>
+        <p>First Time Here? </p>
+        <div id='joinNow'>join now</div>
+        <div className='Img'></div>
+      </div>
+      <div className='rightDivSignIn formContainer'>
+        <h2 id='welcome'>Welcome back 🙂</h2>
+        <p id='signinInstruction'>
+          To continue with your account, please login
+        </p>
 
-      <form action='' id='signinForm'>
-        <div className='emailSection'>
-          <div className='emailIcon'></div>
-          <div className='inputsection'>
-            <label htmlFor='email'>Email Address</label>
-            <input
-              type='text'
-              name='email'
-              id='email'
-              placeholder='example@gmail.com'
-            />
+        <form action='' id='signinForm'>
+          <div className='emailSection'>
+            <div className='emailIcon'></div>
+            <div className='inputsection'>
+              <label htmlFor='email'>Email Address</label>
+              <input
+                type='text'
+                name='email'
+                id='email'
+                placeholder='example@gmail.com'
+              />
+            </div>
           </div>
-        </div>
-        <div className='passwordSection'>
-          <div className='passwordIcon'></div>
-          <div className='inputsection'>
-            <label htmlFor='password'>Password</label>
-            <input
-              type='password'
-              name='password'
-              id='password'
-              placeholder='**********'
-            />
+          <div className='passwordSection'>
+            <div className='passwordIcon'></div>
+            <div className='inputsection'>
+              <label htmlFor='password'>Password</label>
+              <input
+                type='password'
+                name='password'
+                id='password'
+                placeholder='**********'
+              />
+            </div>
           </div>
-        </div>
-        <div className='submitWrapper'>
-          <button type='submit' id='login'>
-            Login
-          </button>
-          <div className='forgotPassword'>Forgot Password?</div>
-        </div>
-      </form>
+          <div className='submitWrapper'>
+            <button type='submit' id='login'>
+              Login
+            </button>
+            <div className='forgotPassword'>Forgot Password?</div>
+          </div>
+        </form>
 
-      <div className='continueWith'>
-        <p>or you can continue with</p>
-        <div className='options'>
-          <div id='apple'></div>
-          <div id='github'></div>
-          <div id='google'></div>
+        <div className='continueWith'>
+          <p>or you can continue with</p>
+          <div className='options'>
+            <div id='apple'></div>
+            <div id='github'></div>
+            <div id='google'></div>
+          </div>
         </div>
       </div>
     </React.Fragment>
