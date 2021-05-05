79379bd0f9a8290015540599eec61fc309357c88
Marvellous Nwachukwu <marvellousn561@gmail.com>
2021-05-05 21:04:33 +0100

    fixed useEffect repeating error

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './scss/task.scss';
 import firebase from '../firebase';
-const Task = () => {
+const Task = ({ taskData }) => {
   //   firebase
   //     .firestore()
   //     .collection('task')
@@ -17,14 +17,11 @@ const Task = () => {
   return (
     <div className='taskPg'>
       <div className='taskDetails'>
-        <h3 className='taskName'>Buy apples from the grocery store</h3>
-        <p className='dueDate'>Set for: Monday, 2:30 pm</p>
-        <p className='dueIn'>Due in: 3hrs, 30mins 15secs</p>
+        <h3 className='taskName'>{taskData.task} </h3>
       </div>
       <div className='taskButtons'>
-        <div id='cancel_btn'></div>
-        <div id='done_btn'></div>
         <div id='delete_btn'></div>
+        <div id='done_btn'></div>
       </div>
     </div>
   );
--- src/pages/body.jsx
+++ src/pages/body.jsx
@@ -1,18 +1,23 @@
 import React, { useRef } from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import '../components/scss/body.scss';
 import Task from '../components/task';
 import firebase from '../firebase';
 
-const Body = (props) => {
+const Body = ({ props }) => {
+  // Hooks
   let input = useRef();
   let greetingText = useRef();
   let [newTask, setNewTask] = useState('');
+  let [allTask, setTask] = useState();
+  let [TaskSaved, updateTaskSaved] = useState(Math.random());
+
   // firebase
   let user = firebase.auth().currentUser;
   let firstName = user.displayName.split(' ')[0];
 
-  const greetUser = (event) => {
+  const greetUser = () => {
     const newDate = new Date();
     const hour = newDate.getHours();
 
@@ -38,6 +43,10 @@ const Body = (props) => {
       .catch((err) => {
         console.log(err);
       });
+
+    setTimeout(() => {
+      updateTaskSaved(Math.random());
+    }, 1000);
   };
 
   const fetchFromFirebase = () => {
@@ -47,30 +56,34 @@ const Body = (props) => {
       .collection(user.uid)
       .get()
       .then((querySnapshot) => {
+        let taskArr = [];
         querySnapshot.forEach((doc) => {
-          console.log(doc.id, ' => ', doc.data());
+          taskArr.push(doc.data());
         });
+        console.log(taskArr);
+        setTask(taskArr);
       });
+
+    console.warn('I ran');
   };
 
   const inputChanged = () => {
     setNewTask(input.current.value);
   };
-
-  setTimeout(() => {
-    if (user) {
-      greetUser();
-      fetchFromFirebase();
-    }
-  }, 100);
+  useEffect(() => {
+    greetUser();
+    fetchFromFirebase();
+  }, [TaskSaved]);
 
   return (
     <div id='motherDiv'>
       <section className='leftSect'>
-        <Task />
-        <Task />
-        <Task />
+        {allTask &&
+          allTask.map((item, index) => {
+            return <Task taskData={item} />;
+          })}
       </section>
+
       <section className='rightSect'>
         <p id='greetingText' ref={greetingText}>
           Greeting's, User;
@@ -89,11 +102,6 @@ const Body = (props) => {
               ref={input}
               onChange={inputChanged}
             />
-            <div className='timerOptions'>
-              <div className='calender'></div>
-              {/* <DatePicker /> */}
-              <div className='clock'></div>
-            </div>
           </form>
         </div>
       </section>
